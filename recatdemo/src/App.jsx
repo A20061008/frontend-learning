9941347dcd3103faf96be82583f0f8787b0e8ab6
A20061008 <amruthbr1@gmail.com>
2025-10-16 09:56:02 +0530

    react-demo-learning

---
+++ recatdemo/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+const app=()=>
+{
+  return(
+    <>
+    <Header/>{" "}
+    <div>
+      <h1>Hello From Reacts!</h1>
+      <p>Myself Amruth,From AIML</p>
+    </div>
+    </>
+  )
+}
+export default app;
